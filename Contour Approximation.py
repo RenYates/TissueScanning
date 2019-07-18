d32fafd11d03d7f938bed73906ff0cad8f5ad81d
Lauren Yates <15ly1@queensu.ca>
2019-07-18 17:44:39 -0400

    Contains functioning corner detection method to find the corners of the slide image. Camera_calibration file is used to calibrate lens for macro imaging.

--- Contour Approximation.py
+++ Contour Approximation.py
@@ -16,8 +16,6 @@ def calc_dist_between_pts (ptA, ptB):
     y_diff = (ptB[1]-ptA[1])**2
     return (math.sqrt(x_diff + y_diff))
 
-
-
 def object_size(conts, width):
 
     # sort the contours from left-to-right and initialize the
@@ -98,11 +96,11 @@ def imaging(img, contour_mask, height, pixelsPerMetric, step_size, x_top_left, y
 
     # normalize the generated photo
     normalized_image = cv2.normalize(imaging_rows, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-    print("Normalized Image pts:", normalized_image)
+    #print("Normalized Image pts:", normalized_image)
     #cv2.imshow('normalized_image', normalized_image)
     #cv2.imwrite('lines.png', normalized_image)
     pixel_lines = cv2.findNonZero(normalized_image)
-    print("normalized pixel_lines: ", pixel_lines)
+    #print("normalized pixel_lines: ", pixel_lines)
     return pixel_lines
 
 def find_slide_contour(img,cnts):
@@ -127,98 +125,88 @@ def find_slide_contour(img,cnts):
             bounding_boxes.append([x, y, width, height])
     return bounding_boxes
 
-def create_hough_lines(img, edges):
-
-    maxLineGap = 100
-    minLineLength = 10
-    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=minLineLength, maxLineGap=maxLineGap)
-    for line in lines:
-        x1, y1, x2, y2 = line[0]
-        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    cv2.imshow('edges', edges)
-    cv2.imshow('lines', img)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-
 def create_contour_mask(img, cnts):
     # create the mask image with all zeros
-    mask = np.zeros(img.shape, np.uint8)
+    mask = np.zeros(img.shape, dtype=np.float32)
     # draw the contour points onto the mask image & fill it in
     cv2.drawContours(mask, [cnts], 0, (255, 255, 255), -1)
     # find the points within the contour
-    pixelpoints = cv2.findNonZero(mask)
+    #pixelpoints = cv2.findNonZero(mask)
     cv2.imshow('mask', mask)
     return mask
 
-def harris_corners_detection(input_image, input_contours):
-    for contour in input_contours:
-        size = cv2.contourArea(contour)
-        rect = cv2.minAreaRect(contour)
-        if size > 1000:
-            gray = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
-            gray = np.float32(gray)
-            mask = np.zeros(gray.shape, dtype=np.float32)
-            cv2.fillPoly(mask, [contour], (255,255,255))
-            dst = cv2.cornerHarris(mask, 5, 3, 0.04)
-            ret, dst = cv2.threshold(dst, 0.1*dst.max(), 255, 0)
-            dst = np.uint8(dst)
-            ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
-            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-            corners = cv2.cornerSubPix(gray, np.float32(centroids), (5,5), (-1,-1), criteria)
-            input_image[dst>0.1*dst.max()] = [0,255,0]
-            cv2.imshow('image',input_image)
-            cv2.waitKey()
-            cv2.destroyAllWindows()
-
-def good_features_corner_detection(input_image, input_contours):
+def find_corners(input_image, input_contours):
+    # convert image to grayscale and change the type to float32
     gray = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
     gray = np.float32(gray)
-    mask = np.ones(gray.shape, dtype=np.float32)
+    # search for the slide contour through the list of contours
     for contour in input_contours:
-            cv2.fillPoly(mask, [contour], [0,0,0])
-    cv2.imshow('mask', mask)
-    corners = cv2.goodFeaturesToTrack(mask, 10, 0.01, 10)
-    corners = np.int0(corners)
+        size = cv2.contourArea(contour)
+        # if the contour is of reasonable size (the slide contour) then create a mask
+        if size > 1000:
+            mask = create_contour_mask(gray, contour)
+    cv2.imshow('contours mask', mask)
+    # find all non-zero points in the form [x,y] (column, row)
+    slide_points = cv2.findNonZero(mask)
+    # convert the points into a matrix to do matrix multiplication
+    matrix_slide_pts = np.asmatrix(slide_points)
+    # 4 corners will be: min(x+y), min(-x-y), min(x-y), min(-x+y)
+    # build an array with the correct + and - signs for multiplication
+    multi_matrix = np.array([[1, -1, 1, -1], [1, -1, -1, 1]])
+    # multiply the slide pts matrix to the multi_matrix
+    result = matrix_slide_pts.dot(multi_matrix)
+    print(result)
 
-    for corner in corners:
-        x, y = corner.ravel()
-        print("x:", x, "y:", y)
-        cv2.circle(input_image, (x,y), 5, (0,255,0), -1)
+    # find the row index of the min value in each column in the result matrix
+    # column 1 is top left, column 2 is bottom right, column 3 is bottom left, column 4 is top right
+    indexes = []
+    for column in range(result.shape[1]):
+        indexes.append(np.argmin(result[:, column]))
 
-    cv2.imshow('good features corners',input_image)
-    cv2.imwrite('good_features_corners.png',input_image)
+    # find the corresponding (x,y) coordinates for each min value index
+    points = []
+    for index in indexes:
+        points.append(slide_points[index][0])
+
+    # plot the found points onto the mask image
+    for point in points:
+        cv2.circle(input_image, (point[0], point[1]), 5, (0, 255, 255), -1)
+
+    cv2.imshow('mask w/ corners', input_image)
+    cv2.imwrite('calculated_corners.jpg',input_image)
     cv2.waitKey()
     cv2.destroyAllWindows()
-
+    return np.asarray(points)
 
 # Calibrate Camera
-#orig_camera_matrix, distortion_coeff, new_camera_mtx = calibrate_camera()
+orig_camera_matrix, distortion_coeff, new_camera_mtx = calibrate_camera()
 
 # Import image
-image = cv2.imread('tissue_slide_black_bg.jpg')
-#image = cv2.undistort(image, orig_camera_matrix, distortion_coeff, new_camera_mtx)
+image = cv2.imread('REIMS_slide_no_flash.jpg')
 # resize the image smaller (for viewing)
-image = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
-copy_image = image.copy()
-good_features_image = image.copy()
-harris_image = image.copy()
+image = cv2.resize(image, (0,0), fx=0.4, fy=0.4)
+image = cv2.undistort(image, orig_camera_matrix, distortion_coeff, None, new_camera_mtx)
+cv2.imshow('undistorted',image)
 cv2.waitKey()
 cv2.destroyAllWindows()
 (max_y, max_x, _) = image.shape
-print ("max x:", max_x,"max y:", max_y)
 
 # Set the step size to 50 micrometer (1000 um = 1mm)
-step_size = 30
+step_size = 50
 # Set the tissue width to 4mm (4000 micrometer)
-tissue_width = 3000
+tissue_width = 4000
 
 # covert image to greyscale, blur it slightly, & threshold it
 img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 #img = cv2.medianBlur(img,7)
-img = cv2.GaussianBlur(img, (5,5), 0)
+#img = cv2.GaussianBlur(img, (5,5), 0)
 #img = cv2.bilateralFilter(img,7,75,75)
+
 cv2.imshow('image',img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
 
 # invert the image
 inv_image = cv2.bitwise_not(img)
@@ -226,10 +214,10 @@ inv_image = cv2.bitwise_not(img)
 # calculate the threshold
 #threshold = cv2.adaptiveThreshold(img, 300, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11, 2)
 #threshold = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,31, 3)
-_, threshold = cv2.threshold(img, 1, 300, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-#inv_threshold = cv2.bitwise_not(threshold)
+_, threshold = cv2.threshold(img, 1, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+#threshold = cv2.bitwise_not(threshold)
 
-#cv2.imshow('threshold',threshold)
+cv2.imshow('threshold',threshold)
 #cv2.imshow('inv_threshold',inv_threshold)
 
 #create_hough_lines(image,edged)
@@ -238,51 +226,96 @@ _, threshold = cv2.threshold(img, 1, 300, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
 # Find contour of the tissue slide
 contours, hierarchy = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-#contours, hierarchy = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-good_features_corner_detection(good_features_image, contours)
-harris_corners_detection(harris_image, contours)
-
 # Draw contours onto display image
-cv2.drawContours(image, contours, -1, (0,0,0), 2)
+contours_copy = image.copy()
+cv2.drawContours(contours_copy, contours, -1, (0,0,0), 2)
 
-cv2.imshow('Contours',image)
-
-# Find the bounding rectangle of each contour, find the largest bounding box to find the slide bounding box
-largest_area = 0
-for c in contours:
-    x,y,w,h = cv2.boundingRect(c)
-    # set a size parameter for the bounding rectangles, and prevent the image border contour from being selected
-    if w < (max_x/2) or w == max_x or h == max_y:
-        continue
-    area = w * h
-    if area > largest_area:
-        bounding_box = (x,y,w,h)
-# plot the largest bounding box onto the image (aka the slide bounding box)
-(x,y,w,h) = bounding_box
-cv2.rectangle(copy_image, (x,y), (x+w, y+h),(0,255,0), 2)
-cv2.imshow('bounding rect', copy_image)
+cv2.imshow('Contours',contours_copy)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
-# rotated bounding boxes
-for c in contours:
-    rotated_rectangle = cv2.minAreaRect(c)
-    box = cv2.boxPoints(rotated_rectangle)
-    (bottom_left, top_left, top_right, bottom_right) = box
-    #print("1:", bottom_left, "2:", top_left, "3:", top_right, "4:", bottom_right)
-    width = calc_dist_between_pts(top_left,top_right)
-    #print("width rotate:", width)
-    height = calc_dist_between_pts(top_left,bottom_left)
-    #print("height rotate:", height)
-    if (width < max_x/2) or width == max_x or height == max_y:
-        continue
-    box = np.int0(box)
-    print(box)
-    cv2.drawContours(copy_image, [box],0,(0,0,255),2)
-    cv2.imshow('bounding rect', copy_image)
+corners_copy = image.copy()
+corners = find_corners(corners_copy, contours)
+print(corners)
+cv2.imshow('original image', image)
 
-cv2.imwrite('contours result.png',copy_image)
+# crop the photo to size of the slide using the min and max x and y corner points
+crop_img = image[np.amin(corners[:,1]):np.amax(corners[:,1]), np.amin(corners[:,0]):np.amax(corners[:,0])]
+grey_crop = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('cropped image',crop_img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+# threshold and find contours of new cropped image to find contours of tissue
+_, cropped_threshold = cv2.threshold(grey_crop, 130, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+# find the contours of the image
+#contours, hierarchy = cv2.findContours(cropped_threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+#lower_boundary = np.array([105,105,105], dtype='uint8')
+#upper_boundary = np.array([192,192,192],dtype='uint8')
+
+#mask = cv2.inRange(crop_img, lower_boundary, upper_boundary)
+#output = cv2.bitwise_and(crop_img, crop_img, mask = mask)
+#cv2.imshow("grey mask", np.hstack([crop_img, output]))
+#cv2.waitKey()
+#cv2.destroyAllWindows()
+
+crop_img_copy = crop_img.copy()
+cv2.drawContours(crop_img_copy, contours, -1, (0,0,0), 2)
+cv2.imshow('cropped contours', crop_img_copy)
+
+cv2.imshow('cropped', cropped_threshold)
+
+# Find the bounding rectangle of each contour, find the largest bounding box to find the slide bounding box
+# largest_area = 0
+# for c in contours:
+#     x,y,w,h = cv2.boundingRect(c)
+#     # set a size parameter for the bounding rectangles, and prevent the image border contour from being selected
+#     if w < (max_x/2) or w == max_x or h == max_y:
+#         continue
+#     area = w * h
+#     if area > largest_area:
+#         bounding_box = (x,y,w,h)
+# plot the largest bounding box onto the image (aka the slide bounding box)
+#(x,y,w,h) = bounding_box
+#cv2.rectangle(copy_image, (x,y), (x+w, y+h),(0,255,0), 2)
+#cv2.imshow('bounding rect', copy_image)
+#cv2.waitKey()
+#cv2.destroyAllWindows()
+
+# rotated_rectangle = cv2.minAreaRect(true_corners)
+# # box = cv2.boxPoints(rotated_rectangle)
+# # (bottom_left, top_left, top_right, bottom_right) = box
+# # #print("1:", bottom_left, "2:", top_left, "3:", top_right, "4:", bottom_right)
+# # width = calc_dist_between_pts(top_left,top_right)
+# # #print("width rotate:", width)
+# # height = calc_dist_between_pts(top_left,bottom_left)
+# # #print("height rotate:", height)
+# # #if (width < max_x/2) or width == max_x or height == max_y:
+# # #    continue
+# # box = np.int0(box)
+# # cv2.drawContours(copy_image, [box],0,(0,255,255),1)
+# # cv2.imshow('bounding rect', copy_image)
+
+# rotated bounding boxes around contour
+# for c in contours:
+#     rotated_rectangle = cv2.minAreaRect(c)
+#     box = cv2.boxPoints(rotated_rectangle)
+#     (bottom_left, top_left, top_right, bottom_right) = box
+#     #print("1:", bottom_left, "2:", top_left, "3:", top_right, "4:", bottom_right)
+#     width = calc_dist_between_pts(top_left,top_right)
+#     #print("width rotate:", width)
+#     height = calc_dist_between_pts(top_left,bottom_left)
+#     #print("height rotate:", height)
+#     if (width < max_x/2) or width == max_x or height == max_y:
+#         continue
+#     box = np.int0(box)
+#     cv2.drawContours(copy_image, [box],0,(255,255,0),1)
+# cv2.imshow('bounding rect', copy_image)
+
+#cv2.imwrite('contours result.png',copy_image)
 
 
 #for c in contours:
---
+++ camera_calibration.py
@@ -0,0 +1,63 @@
+import numpy as np
+import cv2
+import glob
+
+def calibrate_camera():
+
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # prepare object points, like (0,0,0), (1,0,0), ... (6,5,0)
+    object_points = np.zeros((7*7,3), np.float32)
+    object_points[:,:2] = np.mgrid[0:7,0:7].T.reshape(-1,2)
+
+    # Arrays to store object points and image points from all the images
+    obj_points = [] # 3d pt in real world space
+    img_points = [] # 2d pts in image plane
+
+    images = glob.glob('IMG_*.jpg')
+    for image in images:
+        print("INFO: Searching through images")
+        img = cv2.imread(image)
+        img = cv2.resize(img, (0, 0), fx=0.4, fy=0.4)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+        # find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (7,7), None)
+
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            print("INFO: One image found")
+            obj_points.append(object_points)
+
+            corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+            img_points.append(corners2)
+
+            # draw and display the corners
+            chess_img = cv2.drawChessboardCorners(img, (7,7), corners2, ret)
+            #cv2.imshow('gray', gray)
+            #cv2.imshow('img', chess_img)
+            #cv2.waitKey()
+            #cv2.destroyAllWindows()
+
+
+    ret, camera_matrix, distortion_coeff, rotation_vec, translation_vec = cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None)
+
+    h,  w = chess_img.shape[:2]
+
+    new_camera_mtx, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, distortion_coeff, (w , h), 1, (w , h))
+
+    print(roi)
+
+    #undistort the image
+    dst = cv2.undistort(chess_img, camera_matrix, distortion_coeff, None, new_camera_mtx)
+
+    # crop the image
+
+    x, y, w, h = roi
+
+    new_dst = dst[y:y+h, x:x+w]
+    cv2.imwrite('calibration_result.png',dst)
+    return camera_matrix, distortion_coeff, new_camera_mtx
+
+#calibrate_camera()
