f7f95b8cd1999a796cbd542c4b204db063606f7e
Lauren Yates <15ly1@queensu.ca>
2019-08-05 00:44:49 -0400

    Adds test function to run all sample images for results and calculate percent difference and change.

--- Contour Approximation.py
+++ Contour Approximation.py
@@ -6,6 +6,7 @@ import numpy as np
 import imutils
 import cv2
 import math
+import glob
 from camera_calibration import calibrate_camera
 
 def midpoint(ptA, ptB):
@@ -120,7 +121,7 @@ def imaging(input_contour_mask, input_tissue_height, input_tissue_width, pixelsP
         #cv2.circle(line_mask, horiz_right_point, 5, (0,0,0), -1)
         cv2.line(line_mask, horiz_left_point, horiz_right_point, (255,255,255), 1, cv2.LINE_AA)
         step += 1
-    cv2.imshow('mask', line_mask)
+    #cv2.imshow('mask', line_mask)
     cv2.imwrite('line_mask.jpg', line_mask)
     # use numpy.logical_and to determine the pixels where the lines intersect the contour mask
     imaging_rows = cv2.bitwise_and(line_mask, input_contour_mask)
@@ -128,17 +129,16 @@ def imaging(input_contour_mask, input_tissue_height, input_tissue_width, pixelsP
     print(imaging_rows.shape)
     #cv2.imshow('contour lines', imaging_rows)
     cv2.imwrite('contour_lines.jpg', imaging_rows)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    #cv2.waitKey()
+    #cv2.destroyAllWindows()
 
-    # normalize the generated photo
-    #normalized_image = cv2.normalize(imaging_rows, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-    #print("Normalized Image pts:", normalized_image)
-    #cv2.imshow('normalized_image', normalized_image)
-    #cv2.imwrite('lines.png', normalized_image)
-    pixel_lines = cv2.findNonZero(imaging_rows)
-    #print("normalized pixel_lines: ", pixel_lines)
-    return np.vstack(pixel_lines).squeeze()
+    # find the pixel locations of the contour scanning pattern
+    contour_pixel_lines = cv2.findNonZero(imaging_rows)
+
+    # find the pixel locations of the grid scanning pattern
+    grid_pixel_lines = cv2.findNonZero(line_mask)
+
+    return np.vstack(contour_pixel_lines).squeeze(), np.vstack(grid_pixel_lines).squeeze()
 
 def find_slide_contour(img,cnts):
     bounding_boxes = []
@@ -157,8 +157,8 @@ def find_slide_contour(img,cnts):
             #compute the bounding box of the contour and plot it
             (x, y, width, height) = cv2.boundingRect(approx_polygon)
             cv2.rectangle(img,(x,y), (x+width,y+height), (0,0,255), 3)
-            cv2.imshow('slide',img)
-            cv2.waitKey()
+            #cv2.imshow('slide',img)
+            #cv2.waitKey()
             bounding_boxes.append([x, y, width, height])
     return bounding_boxes
 
@@ -170,7 +170,7 @@ def create_contour_mask(img, cnts):
     cv2.drawContours(mask, [cnts], 0, (255, 255, 255), -1)
     # find the points within the contour
     #pixelpoints = cv2.findNonZero(mask)
-    cv2.imshow('mask', mask)
+    #cv2.imshow('mask', mask)
     return mask
 
 def find_corners(input_image, input_contours):
@@ -182,7 +182,7 @@ def find_corners(input_image, input_contours):
         # if the contour is of reasonable size (the slide contour) then create a mask
         if size > image_area/10:
             mask = create_contour_mask(input_image, contour)
-    cv2.imshow('contours mask', mask)
+    #cv2.imshow('contours mask', mask)
     # find all non-zero points in the form [x,y] (column, row)
     slide_points = cv2.findNonZero(mask)
     # convert the points into a matrix to do matrix multiplication
@@ -207,10 +207,10 @@ def find_corners(input_image, input_contours):
     for point in points:
         cv2.circle(input_image, (point[0], point[1]), 5, (0, 255, 255), -1)
 
-    cv2.imshow('mask w/ corners', input_image)
+    #cv2.imshow('mask w/ corners', input_image)
     cv2.imwrite('calculated_corners.jpg',input_image)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    #cv2.waitKey()
+    #cv2.destroyAllWindows()
     return np.asarray(points)
 
 def find_tissue_contour(slide_width, slide_height, slide_threshold):
@@ -279,25 +279,25 @@ def calc_cropped_corners(corner_points):
     cropped_corners = np.asarray([top_left_cropped, bottom_right_cropped, bottom_left_cropped, top_right_cropped])
     return cropped_corners
 
-def main():
+def scan_tissue(old_camera_matrix_file, camera_distortion_coeff_file, new_camera_matrix_file, image_file, step_size_microm):
     # Calibrate Camera
     # orig_camera_matrix, distortion_coeff, new_camera_mtx = calibrate_camera()
-    orig_camera_matrix = np.load("camera_matrix_orig.npy")
-    distortion_coeff = np.load("camera_dist_coeff.npy")
-    new_camera_mtx = np.load("camera_matrix_new.npy")
+    orig_camera_matrix = np.load(old_camera_matrix_file)
+    distortion_coeff = np.load(camera_distortion_coeff_file)
+    new_camera_mtx = np.load(new_camera_matrix_file)
 
     # Import image
-    image = cv2.imread('Tissue Images/tissue20_4.jpg')
+    image = cv2.imread(image_file)
     # apply the calculated camera matrix and distortion coefficients to the image to undistort
     image = cv2.undistort(image, orig_camera_matrix, distortion_coeff, None, new_camera_mtx)
     # display and save undistorted image
-    cv2.imshow('undistorted',image)
-    cv2.imwrite('undistorted.jpg',image)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    #cv2.imshow('undistorted',image)
+    #cv2.imwrite('undistorted.jpg',image)
+    #cv2.waitKey()
+    #cv2.destroyAllWindows()
 
     # Set the step size to 50 micrometer (0.05mm) (1000 um = 1mm)
-    step_size_microm = 50
+    step_size_microm = 500
     # Set the slide width to 25mm (25000 micrometer)
     slide_microm_width = 25000
     # set the slide height to 75mm (75000 micrometer)
@@ -342,7 +342,7 @@ def main():
     grey_crop = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
 
     # cv2.imshow('cropped image',grey_crop)
-    # cv2.imwrite('cropped_photo.jpg', crop_img)
+    cv2.imwrite('cropped_photo.jpg', crop_img)
     # cv2.waitKey()
     # cv2.destroyAllWindows()
     #
@@ -350,7 +350,7 @@ def main():
     _, slide_threshold = cv2.threshold(grey_crop, 130, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
     #cv2.drawContours(crop_img, contours, -1, (0, 0, 255), 2)
-    #cv2.imwrite("slide_threshold.jpg", slide_threshold)
+    cv2.imwrite("slide_threshold.jpg", slide_threshold)
 
     # upscale image for better mm to pixel accuracy
     slide_threshold = cv2.resize(slide_threshold, (0, 0), fx=scaling_factor, fy=scaling_factor)
@@ -368,6 +368,7 @@ def main():
     pixels_per_micrometer_width = slide_pixel_width/slide_microm_width
     pixels_per_micrometer_height = slide_pixel_height/slide_microm_height
     pixels_per_micrometer = (pixels_per_micrometer_height + pixels_per_micrometer_width)/2
+    print("Micrometer per pixel: ", ((slide_microm_height/slide_pixel_height)+(slide_microm_width/slide_pixel_width))/2)
 
     # find the contour of the tissue on the slide
     tissue_contour = find_tissue_contour(slide_pixel_width, slide_pixel_height, slide_threshold)
@@ -376,13 +377,13 @@ def main():
     tissue_mask = create_contour_mask(slide_threshold, tissue_contour)
 
     #cv2.imshow('tissue_mask', tissue_mask)
-    #cv2.imwrite('tissue_mask.jpg', tissue_mask)
+    cv2.imwrite('tissue_mask.jpg', tissue_mask)
 
     # calculate the size of the tissue in pixels
     tissue_width, tissue_height, top_left_pt = calc_tissue_size(tissue_contour)
 
     # calculate the linear scanning coordinates in pixels
-    scan_pixel_img_coord = imaging(tissue_mask, tissue_height, tissue_width, pixels_per_micrometer, step_size_microm, top_left_pt[0], top_left_pt[1])
+    scan_pixel_img_coord, grid_pixel_img_coord = imaging(tissue_mask, tissue_height, tissue_width, pixels_per_micrometer, step_size_microm, top_left_pt[0], top_left_pt[1])
     print("scan pixel image coordinates:\n", scan_pixel_img_coord)
     # convert from image coordinate system to slide coordinate system
     scan_pixel_slide_coord = convert_to_slide_coordinate(scan_pixel_img_coord, corners)
@@ -390,9 +391,53 @@ def main():
     scan_microm_slide_coord = np.divide(scan_pixel_slide_coord, pixels_per_micrometer)
     print("Scan microm coordinates:\n", scan_microm_slide_coord)
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # calculate the size of scan pixels and grid pixels to calculate percentage change/difference
+    contour_pixel_lines_rows = scan_pixel_img_coord.shape[0]
+    print("Contour_pixel_lines_rows:", contour_pixel_lines_rows)
+    print("contour pixels shape: ", scan_pixel_img_coord.shape)
+    grid_pixel_lines_rows = grid_pixel_img_coord.shape[0]
+    print("Grid pixel lines rows: ", grid_pixel_lines_rows)
+    print("grid pixels shape: ", grid_pixel_img_coord.shape)
 
-main()
+    # calculate the difference in size between grid scanning and contour scanning to show improvement
+    percent_difference = ((grid_pixel_lines_rows - contour_pixel_lines_rows)/
+                          ((grid_pixel_lines_rows+contour_pixel_lines_rows)/2)) * 100
+    print("percent difference :", percent_difference)
+    percent_change = ((grid_pixel_lines_rows - contour_pixel_lines_rows)/grid_pixel_lines_rows) * 100
+    print("Percent change: ", percent_change)
+
+    return percent_change, percent_difference
+
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+def gather_results():
+    old_camera_matrix_file = "camera_matrix_orig.npy"
+    camera_distortion_coeff_file = "camera_dist_coeff.npy"
+    new_camera_matrix_file = "camera_matrix_new.npy"
+
+    # Set the step size to 50 micrometer (0.05mm) (1000 um = 1mm)
+    step_size_microm = 50
+
+    # initialize variables
+    total_percent_change = 0
+    total_percent_difference = 0
+    total_num_photos = 0
+
+    images = glob.glob('Tissue Images/*')
+    for image_file in images:
+        print("Image File:", image_file)
+        total_num_photos += 1
+        percent_change, percent_difference = scan_tissue(old_camera_matrix_file, camera_distortion_coeff_file, new_camera_matrix_file, image_file, step_size_microm)
+        total_percent_change += percent_change
+        total_percent_difference += percent_difference
+    average_percent_change = total_percent_change/total_num_photos
+    average_percent_difference = total_percent_difference/total_num_photos
+
+    print("\n\n\n--------------------------------\n\n\n")
+    print("Average Percent Change:", average_percent_change)
+    print("Average Percent Difference:", average_percent_difference)
+
+gather_results()
 
 
